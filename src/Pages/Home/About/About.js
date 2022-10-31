4ac9b0cea477ece01ac7d87311c7d6155362041b
ashiqurras <ashiqurrussel2@gmail.com>
2022-10-31 01:06:45 +0100

    Service area part

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -3,7 +3,7 @@ import person from "../../../assets/images/about_us/person.jpg";
 import parts from "../../../assets/images/about_us/parts.jpg";
 const About = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero my-20">
       <div className="hero-content flex-col lg:flex-row">
         <div className="relative w-1/2">
           <img
@@ -19,7 +19,7 @@ const About = () => {
         </div>
         <div className="w-1/2">
           <p className="text-2xl text-orange-600 font-bold"> About Us</p>
-          <h1 className="text-5xl w-3/2 font-bold">
+          <h1 className="my-5 text-5xl w-3/2 font-bold">
             We are qualified <br /> & of experience <br /> in this field
           </h1>
           <p className="py-6">
