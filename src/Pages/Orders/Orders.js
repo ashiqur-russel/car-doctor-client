dedc3492b52427a7af9571c188a2d7be5b0a2b9f
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-02 23:21:22 +0100

    Load order data from api

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,19 +1,43 @@
 import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../../contexts/AuthProvider";
+import OrderTable from "./OrderTable";
 
 const Orders = () => {
   const { user } = useContext(AuthContext);
-  const [orders, setOrders] = useState({});
-
+  const [orders, setOrders] = useState([]);
+  console.log(orders);
   useEffect(() => {
-    fetch(`http://localhost:5001/orders?email=${user.email}`)
+    fetch(`http://localhost:5001/orders?email=${user?.email}`)
       .then((res) => res.json())
-      .then((data) => setOrders(data))
+      .then((data) => {
+        setOrders(data);
+      })
       .then((err) => console.log(err));
   }, [user?.email]);
   return (
-    <div>
-      <h2 className="text-5xl">You have :{orders.length} orders.</h2>
+    <div className="overflow-x-auto w-full">
+      <h2>Oeders: {orders.length}</h2>
+
+      <table className="table w-full">
+        <thead>
+          <tr className="">
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <th>Customer Name</th>
+            <th>Service</th>
+            <th>Email</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order) => (
+            <OrderTable key={order._id} order={order}></OrderTable>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
