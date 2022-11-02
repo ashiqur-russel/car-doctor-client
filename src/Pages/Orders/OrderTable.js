801e5cd7cd1f4fc8c938eae3e134d868cc631f63
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-03 00:08:47 +0100

    Delete handle added for specefic order

--- src/Pages/Orders/OrderTable.js
+++ src/Pages/Orders/OrderTable.js
@@ -1,23 +1,63 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const OrderTable = ({ order }) => {
-  const { serviceName, price, customer, phone, service } = order;
-  console.log(order);
+  const { serviceName, price, customer, phone, service, _id } = order;
+  const [orderService, setOrderService] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5001/services/${service}`).then((res) =>
+      res.json().then((data) => setOrderService(data))
+    );
+  }, [service]);
+
+  const handleDelete = (id) => {
+    const proceed = window.confirm(
+      "Are you sure, you want to delete this order"
+    );
+
+    if (proceed) {
+      fetch(`http://localhost:5001/orders/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .then((err) => console.log(err));
+    }
+  };
   return (
     <tr>
       <th>
         <label>
-          <input type="checkbox" className="checkbox" />
+          <button
+            className="btn btn-circle btn-outline"
+            onClick={() => handleDelete(_id)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
         </label>
       </th>
       <td>
         <div className="flex items-center space-x-3">
           <div className="avatar">
-            <div className="mask mask-squircle w-12 h-12">
-              <img
-                src="/tailwind-css-component-profile-2@56w.png"
-                alt="Avatar Tailwind CSS Component"
-              />
+            <div className="mask rounded w-24 h-24">
+              {orderService?.img && (
+                <img
+                  src={orderService.img}
+                  alt="Avatar Tailwind CSS Component"
+                />
+              )}
             </div>
           </div>
           <div>
